8882282131324778db39231e431ebdf2f0e081b4
miguelhenao <miguel.henao@utp.edu.co>
2022-02-11 01:10:03 -0500

    Create MenuItem type and const menu-items

---
+++ src/app/helpers/constants/menu-items.ts
@@ -0,0 +1,8 @@
+import { MenuItem } from './types';
+
+export const MENU_ITEMS: MenuItem[] = [
+  { label: 'Registros de descarga', icon: 'text_snippet' },
+  { label: 'Espacios de Trabajo', icon: 'topic' },
+  { label: 'Capas', icon: 'layers' },
+  { label: 'Usuarios', icon: 'group' }
+];
